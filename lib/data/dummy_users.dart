d5efde1c11ff55b023d75b07e3d28c2b3b8d5281
martynsdk <martynsdk@gmail.com>
2022-07-05 16:01:48 -0300

    Initial commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+    id: "1",
+    name: "Maria",
+    email: "maria@alunos.com.br",
+    avatarUrl: "https://cdn.pixabay.com/photo/2022/06/28/13/21/horse-7289708_960_720.jpg"
+  ),
+  '2': User(
+      id: "2",
+      name: "Rafael",
+      email: "rafael@alunos.com.br",
+      avatarUrl: "https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg"
+  ),
+  '3': User(
+      id: "3",
+      name: "Fernanda",
+      email: "fernanda@alunos.com.br",
+      avatarUrl: "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg"
+  ),
+};
